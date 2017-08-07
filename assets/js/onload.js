6cad4c4ae13e0dc86f7a9cc8f8183b78c0f7d0b6
Chris Dimoulis <christopherjamesd@gmail.com>
2017-08-07 14:50:28 -0500

    [CHANGED] Smaller images, set background on ready

--- assets/js/onload.js
+++ assets/js/onload.js
@@ -1,7 +1,32 @@
 $(function(){
+  background();
   // Remove is-loading class from body
-  $body = $('body');
+  var $body = $('body');
   if ($body.length) {
     $body.removeClass('is-loading');
   }
 });
+
+function background() {
+  var _lin_grad = "linear-gradient(60deg,rgba(0,0,0,.25),rgba(0,0,0,.25)),";
+  var _rand = Math.ceil(Math.random()*2);
+  var _img = 'url("images/'+_rand+'_md.jpeg")'
+  var _back_img = _lin_grad + _img
+  var $body = $('body');
+  $body.css('background-image', _back_img)
+  // Change background attributes if image 1
+  if (_rand == 1){
+    $body.css('background-position', 'top right');
+    if (matchMedia) {
+      var _mq = matchMedia('(max-width: 950px)');
+      _mq.addListener(widthChange);
+      widthChange(_mq);
+    }
+  }
+};
+
+function widthChange(mq) {
+  if (mq.matches) {
+    $body.css('background-position', 'top center');
+  }
+};
